cf33e01ef339799260e8e177e4257c9e4229eee9
ferran <ferranllop@gmail.com>
2021-11-18 12:58:32 +0100

    Add Entity to the public export

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { SyncDatabase } from './SyncDatabase.js'
 import { AsyncDatabase } from './AsyncDatabase.js'
-import {Database} from './Database'
+import { Database } from './Database.js'
+import { Entity } from './Entity.js'
 
 export {
+    AsyncDatabase,
     Database,
+    Entity,
     SyncDatabase,
-    AsyncDatabase
 }
